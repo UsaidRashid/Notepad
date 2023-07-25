c2ee372ee87fc96fbd50bd71213c805e4c8031ab
Usaid Rashid <rashidusaid2@gmail.com>
2023-07-25 19:33:59 +0530

    Adding Files

---
+++ mainwindow.h
@@ -0,0 +1,51 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include<QString>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_2_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionAbout_Notepad_triggered();
+
+    void on_actionFont_triggered();
+
+    void on_actionText_Color_triggered();
+
+    void on_actionText_Background_Color_triggered();
+
+    void on_actionWindow_Background_Color_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    QString file_path;
+};
+#endif // MAINWINDOW_H
